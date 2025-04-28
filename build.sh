640d3c02a3fe4ec12db1f31c7feb7f8ecef5c3e9
eyedeekay <hankhill19580@gmail.com>
2025-04-28 19:05:33 -0400

    try an auto-build

--- build.sh
+++ build.sh
@@ -1,23 +1,25 @@
 #! /usr/bin/env sh
 
-git clone https://github.com/element-hq/dendrite
+git clone --depth=1 https://github.com/element-hq/dendrite
 cd dendrite
+git fetch --unshallow
 go build -o bin/ ./cmd/...
+go build -o bin/ ./contrib/...
 
 # Generate a Matrix signing key for federation (required)
-./bin/generate-keys --private-key matrix_key.pem
+#./bin/generate-keys --private-key matrix_key.pem
 
 # Generate a self-signed certificate (optional, but a valid TLS certificate is normally
 # needed for Matrix federation/clients to work properly!)
-./bin/generate-keys --tls-cert server.crt --tls-key server.key
+#./bin/generate-keys --tls-cert server.crt --tls-key server.key
 
 # Copy and modify the config file - you'll need to set a server name and paths to the keys
 # at the very least, along with setting up the database connection strings.
-cp dendrite-sample.yaml dendrite.yaml
+#cp dendrite-sample.yaml dendrite.yaml
 
 # Build and run the server:
-./bin/dendrite --tls-cert server.crt --tls-key server.key --config dendrite.yaml
+./bin/dendrite-demo-i2p --config dendrite.yaml
 
 # Create an user account (add -admin for an admin user).
 # Specify the localpart only, e.g. 'alice' for '@alice:domain.com'
-./bin/create-account --config dendrite.yaml --username alice
+#./bin/create-account --config dendrite.yaml --username alice
